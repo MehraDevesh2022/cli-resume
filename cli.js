462a1205a1b7b400ba19b4235d259d0ee6a5ff64
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-26 20:39:30 +0530

    resume cli project

--- cli.js
+++ cli.js
@@ -1,34 +1,48 @@
 const inquirer = require('inquirer');
 const cp = require('child_process');
+resumeDetails();
+function resumeDetails(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'selection',
+                choices: ['About', 'skills', 'academics', 'Projects']
+            }
 
-inquirer
-    .prompt([
-       {
-            type: 'list',
-            name: 'selection',
-            choices :['About' , 'skills' , 'academics' , 'Projects']
-       }
-
-    ])
-    .then((answers) => {
-        if (answers.selection === 'About'){
-           console.log(`Full Stack Enthusiast | Passionate for Coding |
+        ])
+        .then((answers) => {
+            if (answers.selection === 'About') {
+                console.log(`Full Stack Enthusiast | Passionate for Coding |
 Enthusiastic in Open Source Contribution.`);
-        } else if (answers.selection === 'skills'){
-           console.log('Java' ,'c' ,'JavaScript' , 'React' , 'Nodejs' , 'Express.js' ,'Html' ,'Css');
-        } else if (answers.selection === 'academics'){
-           cp.execSync('start chrome https://photos.google.com/search/doc/photo/AF1QipN6k9Y4KkKpzMu5VLWQT3nWC8CqukvNfcgNp4Za')    
-        } else if (answers.selection === 'Projects'){
-           cp.execSync('start chrome https://github.com/MehraDevesh2022?tab=repositories')
-       }
+            } else if (answers.selection === 'skills') {
+                console.log('Java', 'c', 'JavaScript', 'React', 'Nodejs', 'Express.js', 'Html', 'Css');
+            } else if (answers.selection === 'academics') {
+                cp.execSync('start chrome https://photos.google.com/search/doc/photo/AF1QipN6k9Y4KkKpzMu5VLWQT3nWC8CqukvNfcgNp4Za')
+            } else if (answers.selection === 'Projects') {
+                cp.execSync('start chrome https://github.com/MehraDevesh2022?tab=repositories')
+            }
+            displayGoback();
 
+        })
+
+}
+
+function displayGoback(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'selection',
+                choices: ['Go-Back' ,'Exit']
+            }
+        ])
+    .then((answer) =>{
+        if (answer.selection == 'Go-Back')
+          resumeDetails();
+          else{
+            console.log('resume is closed');
+          }
     })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-            console.log(error);
-        } else {
-            // Something else went wrong
-            console.log('other error');
-        }
-    });
+        
+}
